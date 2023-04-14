a3ebb5444d5ae6fac48c7fc57a424712b852b544
techub <tech4phil@outlook.com>
2023-04-14 17:31:42 +0100

    Initial commit

---
+++ src/main/java/com/philipneo/emailmarketingsystem/EmailmarketingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.philipneo.emailmarketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailmarketingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailmarketingsystemApplication.class, args);
+	}
+
+}
